21d699b2eb87fa2aabf371a41b7e5e38d89de6de
nikos efthias <nikos@mugsoft.io>
2022-07-21 21:28:58 +0300

    start routerify server

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,18 @@ pub use req::*;
 pub type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + Sync + 'a>>;
 pub type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(feature = "routerify")]
+pub async fn start_server<B, E>(addr: std::net::SocketAddr, router: routerify::Router<B, E>)
+where
+	E: Into<Box<dyn std::error::Error + Send + Sync>> + 'static,
+	B: hyper::body::HttpBody + Send + Sync + 'static,
+	B::Error: Into<Box<dyn std::error::Error + Send + Sync>> + 'static,
+	B::Data: Send + Sync + 'static,
+{
+	let service = routerify::RouterService::new(router).expect("failed to start server");
+	hyper::Server::bind(&addr)
+		.serve(service)
+		.await
+		.expect("server failed");
+}
