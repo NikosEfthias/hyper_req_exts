2b75d82b1805aac942f07893cadbc222cc84c186
nikos efthias <nikos@mugsoft.io>
2022-07-28 06:16:20 +0300

    include routerify cors helper

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod req;
 use std::{fmt::Display, future::Future, pin::Pin};
-
+#[cfg(feature = "routerify")]
+mod routerify_exts;
 pub use hyper;
 use hyper::{body::HttpBody, Body};
 pub use req::*;
@@ -8,22 +9,7 @@ pub type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + Sync + 'a>>;
 pub type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 pub type Result<T> = std::result::Result<T, Error>;
 #[cfg(feature = "routerify")]
-pub use routerify;
-
-#[cfg(feature = "routerify")]
-pub async fn start_server<B, E>(addr: std::net::SocketAddr, router: routerify::Router<B, E>)
-where
-	E: Into<Box<dyn std::error::Error + Send + Sync>> + 'static,
-	B: hyper::body::HttpBody + Send + Sync + 'static,
-	B::Error: Into<Box<dyn std::error::Error + Send + Sync>> + 'static,
-	B::Data: Send + Sync + 'static,
-{
-	let service = routerify::RouterService::new(router).expect("failed to start server");
-	hyper::Server::bind(&addr)
-		.serve(service)
-		.await
-		.expect("server failed");
-}
+pub use routerify_exts::*;
 
 pub trait IntoResponse
 {
---
+++ src/routerify_exts.rs
@@ -0,0 +1,31 @@
+use hyper::{header::HeaderValue, Body, Response};
+pub use routerify;
+use tap::Tap;
+pub async fn start_server<B, E>(addr: std::net::SocketAddr, router: routerify::Router<B, E>)
+where
+	E: Into<Box<dyn std::error::Error + Send + Sync>> + 'static,
+	B: hyper::body::HttpBody + Send + Sync + 'static,
+	B::Error: Into<Box<dyn std::error::Error + Send + Sync>> + 'static,
+	B::Data: Send + Sync + 'static,
+{
+	let service = routerify::RouterService::new(router).expect("failed to start server");
+	hyper::Server::bind(&addr)
+		.serve(service)
+		.await
+		.expect("server failed");
+}
+pub async fn cors_allow_all(mut resp: Response<Body>) -> crate::Result<Response<Body>>
+{
+	resp.headers_mut().tap_mut(|it| {
+		it.insert("Access-Control-Allow-Origin", HeaderValue::from_static("*"));
+		it.insert(
+			"Access-Control-Allow-Headers",
+			HeaderValue::from_static("*"),
+		);
+		it.insert(
+			"Access-Control-Allow-Methods",
+			HeaderValue::from_static("*"),
+		);
+	});
+	Ok(resp)
+}
